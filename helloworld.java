ad057b4192156ce7e32ed4bebd492c6624919ca1
anjireddy <55525628+anjireddy1624@users.noreply.github.com>
2019-09-20 13:24:21 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,10 @@
 class helloworld
 {
-public static void main(String [] ar)
+public static void main(String [] ar) 
 {
+  int a =30;
+  int b =40;
 System.out.println("hello java");
+  System.out.println((c=a+b));
 }
 }
